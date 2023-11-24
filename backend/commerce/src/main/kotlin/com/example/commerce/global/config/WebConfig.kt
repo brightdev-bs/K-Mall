258785adff805dc2ab53f2c506ad1b265a8beb81
Byeongsu KIM <brightdev.bs@gmail.com>
2023-11-25 00:26:47 +0900

    cors 문제 해결

---
+++ backend/commerce/src/main/kotlin/com/example/commerce/global/config/WebConfig.kt
@@ -0,0 +1,27 @@
+package com.example.commerce.global.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig: WebMvcConfigurer {
+
+    @Value("\${front-server}")
+    lateinit var FRONT_SERVER: String
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(FRONT_SERVER)
+            .allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.HEAD.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.PATCH.name(),
+                HttpMethod.DELETE.name()
+            );
+    }
+}
