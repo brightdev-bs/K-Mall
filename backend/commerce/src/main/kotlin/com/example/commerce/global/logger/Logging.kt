1db2096e6779c8d0695bfd9602d518caeafcd893
Byeongsu KIM <brightdev.bs@gmail.com>
2023-12-08 21:05:10 +0900

    Revert "로거 팩토리 적용"
    This reverts commit 476f9feb5b8fff568662eb23f3537d5affc231a4.

--- backend/commerce/src/main/kotlin/com/example/commerce/global/logger/Logging.kt
+++
@@ -1,9 +0,0 @@
-package com.example.commerce.global.logger
-
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-interface Logging {
-    val logger: Logger
-        get() = LoggerFactory.getLogger(javaClass)
-}
