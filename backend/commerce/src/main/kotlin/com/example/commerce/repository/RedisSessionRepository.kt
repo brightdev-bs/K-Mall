dddd42532d8143636eed94decdf9aeefdcf04365
Byeongsu KIM <brightdev.bs@gmail.com>
2023-12-08 21:18:26 +0900

    Redis Session TTL 추가

--- backend/commerce/src/main/kotlin/com/example/commerce/repository/RedisSessionRepository.kt
+++ backend/commerce/src/main/kotlin/com/example/commerce/repository/RedisSessionRepository.kt
@@ -2,6 +2,8 @@ package com.example.commerce.repository
 
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Repository
+import java.time.Duration
+import java.util.concurrent.TimeUnit
 
 @Repository
 class RedisSessionRepository(
@@ -12,6 +14,7 @@ class RedisSessionRepository(
 
     fun setSession(sessionId: String) {
         redisTemplate.opsForSet().add(SESSION_PREFIX, sessionId)
+        redisTemplate.expire(SESSION_PREFIX, 3, TimeUnit.DAYS)
     }
 
     fun isExistedSession(sessionId: String): Boolean {
