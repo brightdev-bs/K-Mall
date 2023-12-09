97de08a2799edd48802c83dab0a86776a45f07ac
Byeongsu KIM <brightdev.bs@gmail.com>
2023-12-10 03:37:34 +0900

    redis session 발급 및 저장 로직

--- backend/commerce/src/main/kotlin/com/example/commerce/global/filter/SessionFilter.kt
+++ backend/commerce/src/main/kotlin/com/example/commerce/global/filter/SessionFilter.kt
@@ -1,6 +1,6 @@
 package com.example.commerce.global.filter
 
-import com.example.commerce.global.logger.logger
+import com.example.commerce.global.utils.getSessionFromCookie
 import com.example.commerce.repository.RedisSessionRepository
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
@@ -9,23 +9,23 @@ import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
 // Filter -> 요청, 응답 총 2번, OncePerRequestFilter -> 1번
+
 @Component
 class SessionFilter(
     private val redisSessionRepository: RedisSessionRepository,
 ): OncePerRequestFilter() {
 
-    val log = logger()
+    val log = logger
 
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        val session = request.getSession(true)
 
-        val sessionId = session.id
-        if (!redisSessionRepository.isExistedSession(sessionId)) {
-            redisSessionRepository.setSession(sessionId)
+        val session = getSessionFromCookie(request)
+        if (session == null) {
+            val sessionId = request.getSession(true).id
         }
 
         filterChain.doFilter(request, response)
